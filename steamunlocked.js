3f03f58e04f9782de4e27d127696f90ee0304852
Ksp4r <chris.shizas+git@outlook.com>
2026-09-09 12:19:51 +0930

    Update steamunlocked.js

--- steamunlocked.js
+++ steamunlocked.js
@@ -1,5 +1,8 @@
 const style = document.createElement('style');
 style.textContent = `
+.steam-link{
+    font-size: 1.5rem !important;
+}
 .auto-link{
     color: lightgrey !important;
     font-size: 0.5rem !important;
@@ -13,9 +16,17 @@ style.textContent = `
 document.head.append(style);
 const u=new URL(location.href);
 u.searchParams.set('auto','true');
-const a=document.createElement('a');
+const container = document.createElement('div');
+Object.assign(container.style, {position:'fixed', top:'10px', left:'10px', zIndex:99999, font:'14px sans-serif'});
+document.body.append(container);
+const a = document.createElement('a');
 a.classList.add('auto-link');
 a.href=u.href;
 a.textContent = `(${document.querySelector(".su-hchip--size").textContent}) ${document.querySelector(".su-hero__title").textContent}`;
-Object.assign(a.style,{position:'fixed',top:'10px',left:'10px',zIndex:99999,padding:'10px',background:'#fff',color:'#00e',border:'2px solid #888',borderRadius:'6px',font:'14px sans-serif'});
-document.body.append(a);
+Object.assign(a.style,{padding:'10px',background:'#fff',color:'#00e',border:'2px solid #888',borderRadius:'6px',font:'14px sans-serif'});
+const b = document.createElement('a');
+b.classList.add('steam-link');
+b.href = `www.steam.storepowered.com/search?term=${URIEncode(document.title.split(" Free Download")[0])}`;
+b.setAttribute('target','_blank');
+b.textContent = "Steam Page";
+container.append(a, b);
